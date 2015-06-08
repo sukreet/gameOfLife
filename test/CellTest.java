5d29757b81e5999afb4236e22a32f4c2eb232260
sukreet <sukreetr@thoughtworks.com>
2015-06-08 12:00:42 +0530

    Passed spec for checking if cell is not alive

--- test/CellTest.java
+++ test/CellTest.java
@@ -12,5 +12,12 @@ public class CellTest {
         assertEquals(1, acctualCellState);
     }
 
-   
+    @Test
+    public void checkIfCellisNotAlive() {
+        Cell cell = new Cell(0);
+
+        int acctualCellState = cell.isAlive();
+
+        assertEquals(0, acctualCellState);
+    }
 }
